579cb062ba7608072ec59d0b545b0cce088abd20
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-25 19:02:31 -0500

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { animals } = require('./data/animals.json');
+//line
 
 // app.get('/api/animals', (req, res) => {
 //     res.json(animals);
